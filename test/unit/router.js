cd79849f84b2e7e57d3e015bb92c548f476d6a03
Shane Holloway <shane.holloway@ieee.org>
2018-02-26 19:11:08 -0700

    TEST: Added distinctiion of discover channel versus discover route

--- test/unit/router.js
+++ test/unit/router.js
@@ -20,7 +20,7 @@ describe @ 'Router', @=> ::
 
   describe @ 'messages to targets', @=> ::
 
-    it @ 'should call handler if target does exist', @=>> :: 
+    it @ 'should call handler if target does exist', @=>> ::
       hub.local.registerTarget @ 'a-tgt', (pkt, pktctx) => ::
         const {id_route, id_target} = pkt
         log @ 'tgt recv', @{} id_route, id_target, body: pkt.json()
@@ -34,7 +34,7 @@ describe @ 'Router', @=> ::
         @[] 'tgt recv', @{} id_route: '$unit$', id_target: 'a-tgt', body: @{} msg: 'a message'
 
 
-    it @ 'should be able to lookup logical routes and targets', @=>> :: 
+    it @ 'should be able to lookup logical routes and targets', @=>> ::
       const tgt_id = new Date().toISOString()
       const logical = @{}
         'a-logical-tgt': @{} id_route: '$unit$', id_target: tgt_id
@@ -60,7 +60,7 @@ describe @ 'Router', @=> ::
 
 
   describe @ 'handles undeliverables', @=> ::
-    it @ 'should call channel.undeliverable if route does not exist', @=>> :: 
+    it @ 'should call channel.undeliverable if route does not exist', @=>> ::
       await test_chan.send @:
         id_route: 'dne-route'
         id_target: 'dne-tgt'
@@ -70,7 +70,7 @@ describe @ 'Router', @=> ::
         @[] 'undeliverable', @{} mode: 'route', id_route: 'dne-route', id_target: 'dne-tgt'
 
 
-    it @ 'should call channel.undeliverable if target does not exist', @=>> :: 
+    it @ 'should call channel.undeliverable if target does not exist', @=>> ::
       await test_chan.send @:
         id_route: '$unit$'
         id_target: 'dne-tgt'
@@ -82,7 +82,7 @@ describe @ 'Router', @=> ::
 
   describe @ 'discovery', @=> ::
 
-    it @ 'should call router.routeDiscovery if route does not exist', @=>> :: 
+    it @ 'should call router.routeDiscovery if route does not exist', @=>> ::
       hub.router.routeDiscovery.push @
         ({id_route, key}) => ::
           log @ 'discovery', @{} key, id_route
@@ -98,7 +98,7 @@ describe @ 'Router', @=> ::
         @[] 'undeliverable', @{} mode: 'route', id_route: 'dne-route', id_target: 'dne-tgt'
 
 
-    it @ 'should use router.routeDiscovery answer if route found', @=>> :: 
+    it @ 'should use router.routeDiscovery channel answer if route found', @=>> ::
       hub.router.routeDiscovery.push @
         ({id_route, key}) => ::
           log @ 'discovery', @{} key, id_route
@@ -115,7 +115,24 @@ describe @ 'Router', @=> ::
         @[] 'sent pkt', @{} id_route: 'dne-route', id_target: 'dne-tgt'
 
 
-    it @ 'should timeout router.routeDiscovery if no response', @=>> :: 
+    it @ 'should use router.routeDiscovery function answer if route found', @=>> ::
+      hub.router.routeDiscovery.push @
+        ({id_route, key}) => ::
+          log @ 'discovery', @{} key, id_route
+          return ({id_route, id_target}) => ::
+            log @ 'route pkt', @{} id_route, id_target
+
+      await test_chan.send @:
+        id_route: 'dne-route'
+        id_target: 'dne-tgt'
+        body: @{} msg: 'a message'
+
+      expect(log.calls).to.be.deep.equal @#
+        @[] 'discovery', @{} key: 'dne-route', id_route: 'dne-route'
+        @[] 'route pkt', @{} id_route: 'dne-route', id_target: 'dne-tgt'
+
+
+    it @ 'should timeout router.routeDiscovery if no response', @=>> ::
       hub.router._discoveryTimeout = () => ::
         log @ 'createDiscoveryTimeout'
         return ({id_route, key}) => ::
